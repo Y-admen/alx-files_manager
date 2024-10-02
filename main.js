dab8ccf4ed4374df4043d38f6180f53e7541be74
Y-admen <yassminash305@gmail.com>
2024-10-03 02:28:24 +0300

    First API

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-import redisClient from './utils/redis';
+import dbClient from './utils/db';
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+const waitConnection = () => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        const repeatFct = async() => {
+            setTimeout(async() => {
+                i += 1;
+                if (i >= 10) {
+                    reject();
+                } else if (!await dbClient.isAlive()) {
+                    repeatFct();
+                } else {
+                    resolve();
+                }
+            }, 1000);
+        };
+        repeatFct();
+    });
+};
 
 (async() => {
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
-
-    setTimeout(async() => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000 * 10)
+    console.log(await dbClient.isAlive());
+    await waitConnection();
+    console.log(await dbClient.isAlive());
+    console.log(await dbClient.nbUsers());
+    console.log(await dbClient.nbFiles());
 })();
--- utils/db.js
+++ utils/db.js
@@ -4,31 +4,44 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 class DBClient {
-  constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}`;
 
-    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.client.connect().then(() => {
-      this.db = this.client.db(database);
-    }).catch((err) => {
-      console.error('MongoDB Client Error', err);
-    });
-  }
+        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.client.connect().then(() => {
+            this.db = this.client.db(database);
+        }).catch((err) => {
+            console.error('MongoDB Client Error', err);
+        });
+    }
 
-  isAlive() {
-    return this.client.isConnected();
-  }
+    async isAlive() {
+        try {
+            await this.client.db().admin().ping();
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
 
-  async nbUsers() {
-    return this.db.collection('users').countDocuments();
-  }
+    async nbUsers() {
+        if (!this.db) {
+            await this.client.connect();
+            this.db = this.client.db(process.env.DB_DATABASE || 'files_manager');
+        }
+        return this.db.collection('users').countDocuments();
+    }
 
-  async nbFiles() {
-    return this.db.collection('files').countDocuments();
-  }
+    async nbFiles() {
+        if (!this.db) {
+            await this.client.connect();
+            this.db = this.client.db(process.env.DB_DATABASE || 'files_manager');
+        }
+        return this.db.collection('files').countDocuments();
+    }
 }
 
 const dbClient = new DBClient();
