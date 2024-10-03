e2d4824fc190463d82ba1efad33dba33ebbf5785
Y-admen <yassminash305@gmail.com>
2024-10-03 05:15:42 +0300

    2. First API

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
 import dbClient from './utils/db';
 
-process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-});
-
 const waitConnection = () => {
     return new Promise((resolve, reject) => {
         let i = 0;
         const repeatFct = async() => {
-            setTimeout(async() => {
+            await setTimeout(() => {
                 i += 1;
                 if (i >= 10) {
-                    reject();
-                } else if (!await dbClient.isAlive()) {
-                    repeatFct();
+                    reject()
+                } else if (!dbClient.isAlive()) {
+                    repeatFct()
                 } else {
-                    resolve();
+                    resolve()
                 }
             }, 1000);
         };
         repeatFct();
-    });
+    })
 };
 
 (async() => {
-    console.log(await dbClient.isAlive());
+    console.log(dbClient.isAlive());
     await waitConnection();
-    console.log(await dbClient.isAlive());
+    console.log(dbClient.isAlive());
     console.log(await dbClient.nbUsers());
     console.log(await dbClient.nbFiles());
 })();
